18bb8baabdcf0cf79784eebdbdd802a173ab4aa7
Damian Ruiz <nrt.damian@gmail.com>
2020-03-09 21:04:04 -0400

    branch02

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Search from './components/Search'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Let's try it one more time</Text>
+      <Search onChange={()=>{}}></Search>
     </View>
   );
 }
